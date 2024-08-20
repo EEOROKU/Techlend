3d923896d77aebc6a027f7b0a4200cce71d809a3
EEOROKU <113193015+EEOROKU@users.noreply.github.com>
2024-08-19 23:50:08 -0300

    Original

---
+++ script.js
@@ -0,0 +1,17 @@
+// Load window at top
+window.onload = function() {
+    window.scrollTo(0, 0);
+}
+
+// Opens Navigation from the right
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.transform = "translateX(0)";
+}
+
+// Closes Navigation, sliding it out to the right
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.transform = "translateX(100%)";
+}
+
