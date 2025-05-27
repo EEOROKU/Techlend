aa40838d670bccdd85e50a79831b11213410fbff
Eric Oroku <113193015+EEOROKU@users.noreply.github.com>
2025-05-27 20:18:12 -0300

    Add files via upload
    all latest updates

--- mailing.php
+++ mailing.php
@@ -13,20 +13,14 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
     $email = $_POST['email'];
-    $day = $_POST['day'];
-    $time = $_POST['time'];
-    $location = $_POST['location'];
-    $software = $_POST['software'];
 
-    $sql = "INSERT INTO pickup (name, email, day, time, location, software) VALUES ('$name', '$email','$day', '$time', '$location','$software')";
-    
+    $sql = "INSERT INTO Newsletter (email) VALUES ('$email')";
     $url = "https://lend.deets.technology/";
     $font = "https://use.fontawesome.com/releases/v5.15.4/css/all.css";
+    $insta = "https://www.instagram.com/deets.tech/";
     if ($conn->query($sql) === TRUE) {
-        echo "
-        <html>
+        echo "<html>
         <head>
         <meta charset=\"UTF-8\">
         <title>Tech lend</title>
@@ -52,13 +46,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <body>
             <div class=\"center-content\">
                 <a href=\"index.html\"><img src=\"img/logo_lend2.png\" width=\"500px\" height=\"500px\" class=\"logo\" alt=\"\"></a>
-                <h1>Rented successfully!!!</h1>
+                <h1>You have been added to our mailing list successfully!!!😁</h1>
                 <p><a href='$url'>HOMEPAGE</a></p>
-                <p>We will keep in touch!</p>
+                <p>We will keep giving you updates about our latest devices and special offers!!!</p>
             </div>
+            <script>
+            setTimeout(function() { window.location.href = '$insta'; }, 2000); // 2000 milliseconds = 2 seconds
+            </script>
+
         </body>
-        </html>
-        ";
+        </html>";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
